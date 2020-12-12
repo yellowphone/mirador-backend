9ec097b40c74a5e7329d34293dc2f4afd9851609
geomin76 <geom@vt.edu>
2020-12-12 15:23:09 -0500

    Changing port and cors

--- server.js
+++ server.js
@@ -9,9 +9,9 @@ dotenv.config();
 
 
 const app = express();
+app.use(cors())
 app.use(bodyParser.json());
-app.use(cors({origin: 'http://localhost:3000'}));
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 
 const { PrismaClient } = require('@prisma/client');
